c8967695e32a0a4a7b1f8626c47ea0fafb5a8aae
R1nge <r1ngegamedev@gmail.com>
2025-09-04 10:50:56 +0300

    ecs

--- Assets/Scripts/MorpehECS/SpiderJobECS.cs
+++ Assets/Scripts/MorpehECS/SpiderJobECS.cs
@@ -6,16 +6,60 @@ namespace MorpehECS
 {
     public struct SpiderJobECS : IJobParallelFor
     {
-        [ReadOnly]
-        public NativeFilter entities;
+        [ReadOnly] public NativeFilter entities;
         public NativeStash<SpiderMoveComponent> moveComponent;
-        
-        public void Execute(int index) {
+
+        public void Execute(int index)
+        {
             var entity = this.entities[index];
-        
+
             ref var component = ref this.moveComponent.Get(entity);
-            
-            component.Value += 1;
+
+            var screenPoint = _camera.WorldToScreenPoint(move.transform.position);
+            var speed = move.moveSpeed;
+            TickTimer(ref move, deltaTime);
+            ChangeDirection(ref move);
+            HandleOffScreen(ref move, screenPoint);
+            RotateTowardsTarget(ref move, deltaTime);
+        }
+
+        private void TickTimer(ref SpiderMoveComponent moveComponent, float delta)
+        {
+            moveComponent.changeDirectionCooldown -= delta;
+        }
+
+        private void ChangeDirection(ref SpiderMoveComponent moveComponent)
+        {
+            if (moveComponent.changeDirectionCooldown <= 0)
+            {
+                var newAngle = Entrypoint.Instance.Random.NextFloat(-90f, 90f);
+                Quaternion rotation = Quaternion.AngleAxis(newAngle, Vector3.forward);
+                moveComponent.direction = rotation * moveComponent.direction;
+
+                moveComponent.changeDirectionCooldown = Entrypoint.Instance.Random.NextFloat(1f, 5f);
+            }
+        }
+
+        private void HandleOffScreen(ref SpiderMoveComponent moveComponent, Vector3 screenPoint)
+        {
+            if (screenPoint.x < 0 && moveComponent.direction.x < 0 ||
+                (screenPoint.x > _screenWidth && moveComponent.direction.x > 0))
+            {
+                moveComponent.direction = new Vector2(-moveComponent.direction.x, moveComponent.direction.y);
+            }
+
+            if (screenPoint.y < 0 && moveComponent.direction.y < 0 ||
+                (screenPoint.y > _screenHeight && moveComponent.direction.y > 0))
+            {
+                moveComponent.direction = new Vector2(moveComponent.direction.x, -moveComponent.direction.y);
+            }
+        }
+
+        private void RotateTowardsTarget(ref SpiderMoveComponent moveComponent, float delta)
+        {
+            var targetRotation = Quaternion.LookRotation(Vector3.forward, moveComponent.direction);
+            moveComponent.transform.rotation = Quaternion.RotateTowards(moveComponent.transform.rotation,
+                targetRotation, moveComponent.rotateSpeed * delta);
         }
     }
 }
--- Assets/Scripts/MorpehECS/SpiderMoveSystem.cs
+++ Assets/Scripts/MorpehECS/SpiderMoveSystem.cs
@@ -2,6 +2,7 @@
 using Scellecs.Morpeh.Native;
 using Scellecs.Morpeh.Systems;
 using Unity.IL2CPP.CompilerServices;
+using Unity.Jobs;
 using UnityEngine;
 
 namespace MorpehECS
@@ -39,56 +40,12 @@ namespace MorpehECS
 
         public override void OnUpdate(float deltaTime)
         {
-            foreach (var entity in _moveFilter)
-            {
-                ref var move = ref _spiderMoveStash.Get(entity);
-                var screenPoint = _camera.WorldToScreenPoint(move.transform.position);
-                var speed = move.moveSpeed;
-                TickTimer(ref move, deltaTime);
-                ChangeDirection(ref move);
-                HandleOffScreen(ref move, screenPoint);
-                RotateTowardsTarget(ref move, deltaTime);
-                move.transform.Translate(move.transform.up * speed * deltaTime, Space.World);
-            }
-        }
-
-        private void TickTimer(ref SpiderMoveComponent moveComponent, float delta)
-        {
-            moveComponent.changeDirectionCooldown -= delta;
-        }
-
-        private void ChangeDirection(ref SpiderMoveComponent moveComponent)
-        {
-            if (moveComponent.changeDirectionCooldown <= 0)
-            {
-                var newAngle = Entrypoint.Instance.Random.NextFloat(-90f, 90f);
-                Quaternion rotation = Quaternion.AngleAxis(newAngle, Vector3.forward);
-                moveComponent.direction = rotation * moveComponent.direction;
-
-                moveComponent.changeDirectionCooldown = Entrypoint.Instance.Random.NextFloat(1f, 5f);
-            }
-        }
-
-        private void HandleOffScreen(ref SpiderMoveComponent moveComponent, Vector3 screenPoint)
-        {
-            if (screenPoint.x < 0 && moveComponent.direction.x < 0 ||
-                (screenPoint.x > _screenWidth && moveComponent.direction.x > 0))
-            {
-                moveComponent.direction = new Vector2(-moveComponent.direction.x, moveComponent.direction.y);
-            }
-
-            if (screenPoint.y < 0 && moveComponent.direction.y < 0 ||
-                (screenPoint.y > _screenHeight && moveComponent.direction.y > 0))
-            {
-                moveComponent.direction = new Vector2(moveComponent.direction.x, -moveComponent.direction.y);
-            }
-        }
-
-        private void RotateTowardsTarget(ref SpiderMoveComponent moveComponent, float delta)
-        {
-            var targetRotation = Quaternion.LookRotation(Vector3.forward, moveComponent.direction);
-            moveComponent.transform.rotation = Quaternion.RotateTowards(moveComponent.transform.rotation,
-                targetRotation, moveComponent.rotateSpeed * delta);
+            var parallelJob = new SpiderJobECS {
+                entities = _moveFilter,
+                moveComponent = _spiderMoveStash.AsNative(),
+            };
+            
+            World.JobHandle = parallelJob.Schedule(_moveFilter.length, 64, World.JobHandle);
         }
     }
 }
